6cbc9c3a2cf68d304946116f80d4b7aad5e5f93c
arioprima <arioprima2@gmail.com>
2023-10-13 15:58:07 +0700

    create project and setup project

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"status":  "success",
+		})
+	})
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
